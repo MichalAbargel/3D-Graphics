188f19f6d599814a50386143da17838638b3ac87
MichalAbargel <73194579+MichalAbargel@users.noreply.github.com>
2021-06-17 13:28:58 +0300

    Update Camera.java

--- ProjectJava-main/src/elements/Camera.java
+++ ProjectJava-main/src/elements/Camera.java
@@ -4,6 +4,10 @@ import primitives.Point3D;
 import primitives.Ray;
 import primitives.Vector;
 
+import java.awt.color.ICC_ColorSpace;
+import java.util.ArrayList;
+import java.util.List;
+
 import static primitives.Util.isZero;
 
 
@@ -11,6 +15,7 @@ import static primitives.Util.isZero;
 
 public class Camera {
 
+    public static  int SuperSamplingNumber=3;
     Point3D _p0;
     Vector _vTo;
     Vector _vUp;
@@ -107,17 +112,16 @@ public class Camera {
         double ry = _height / nY;
         double rx = _width / nX;
 
-       /* double xj = (i - nX / 2.0) * rx + rx / 2.0;
-        double yi = (j - nY / 2.0) * ry + ry / 2.0;*/
 
-       double yi = -(((i - ((nY - 1) / 2d)))*ry);
+
+        double yi = -(((i - ((nY - 1) / 2d)))*ry);
         double xj = ((j - ((nX - 1) / 2d)))*rx;
 
         Point3D Pij = pc ;
 
 
         if (!isZero(xj)) {
-           Pij=Pij.add(_vRight.scale(xj));
+            Pij=Pij.add(_vRight.scale(xj));
         }
 
 
@@ -129,7 +133,77 @@ public class Camera {
         Vector v = Pij.subtract(_p0);
 
 
-        return new Ray(_p0, v);
+        return new Ray(_p0, v.normalize());
 
+    }
+
+    public List<Ray> constructRayThroughPixelSuperSampling(int nX, int nY, int x, int y){
+        List<Ray> rays=new ArrayList<Ray>();
+
+        for (int i=0; i<SuperSamplingNumber;i++){
+            for (int j=0; j<SuperSamplingNumber;j++){
+
+                rays.add(this.constructRayThroughPixelGrid(nX,nY,x,y,i,j));
+            }
+        }
+        return rays;
+    }
+    public Ray constructRayThroughPixelGrid(int nX, int nY, int x, int y, int i, int j){
+
+        Point3D pc= this.getCenterOfPixel(nX,nY,y,x);
+
+        double Rx=_width/nX/SuperSamplingNumber;
+        double Ry=_height/nY/SuperSamplingNumber;
+        Point3D p=this.getCenterOfPixelP(pc,Rx, Ry,i, j);
+        return new Ray(_p0,p.subtract(_p0).normalize());
+    }
+
+
+    public Point3D getCenterOfPixel(int nX, int nY, int j, int i){
+        Point3D pc = _p0.add(_vTo.scale(_distance));
+
+        if (isZero(nX) || isZero(nY)) {
+            throw new IllegalArgumentException("can not be 0");
+        }
+
+        double ry = _height / nY;
+        double rx = _width / nX;
+
+
+
+        double yi = -(((i - ((nY - 1) / 2d)))*ry);
+        double xj = ((j - ((nX - 1) / 2d)))*rx;
+
+        Point3D Pij = pc ;
+
+
+        if (!isZero(xj)) {
+            Pij=Pij.add(_vRight.scale(xj));
+        }
+
+
+        if (!isZero(yi)) {
+            Pij=Pij.add(_vUp.scale(yi));
+        }
+        return Pij;
+    }
+
+    public Point3D getCenterOfPixelP(Point3D pc, double Rx, double Ry, int i, int j){
+
+        double yi = (i - (SuperSamplingNumber / 2.0))*Ry+Ry/2.0;
+        double xj = (j - (SuperSamplingNumber / 2.0))*Rx+Rx/2.0;
+
+        Point3D Pij = pc ;
+
+
+        if (!isZero(xj)) {
+            Pij=Pij.add(_vRight.scale(xj));
+        }
+
+
+        if (!isZero(yi)) {
+            Pij=Pij.add(_vUp.scale(-yi));
+        }
+        return Pij;
     }
 }
